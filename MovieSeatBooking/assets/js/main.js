6cf47666f52499b11755453a58b866e1e0108f93
william-italia <williamitalia70@outlook.com>
2023-07-22 11:45:07 -0300

    up

---
+++ MovieSeatBooking/assets/js/main.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('Movie')
+
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.select');
+    const SelectedSeatsCount = selectedSeats.length;
+    count.innerText = SelectedSeatsCount;
+    total.innerText = SelectedSeatsCount * ticketPrice;
+}
+
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+container.addEventListener('click', (e) => {
+    const el = e.target;
+    if(el.classList.contains('seat') && !el.classList.contains('occupied')) {
+        el.classList.toggle('select');
+
+        updateSelectedCount();
+    }
+})
